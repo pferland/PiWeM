5a1759fd61c0eb4c3e0c26e6f61ead3d9d319d79
Phil <pferland@randomintervals.com>
2016-08-14 11:49:29 -0400

    rename of class
    renamed pimon to PiWeM since that is what it is.

--- PiWeM/PiWeM.py
+++ PiWeM/PiWeM.py
@@ -6,8 +6,10 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from dht11 import dht11
 
-class PIMON:
+class PIWEM:
     loop_int = 0
+    take_picture_flag = 1
+    verbose = 0
     pin = 0
     brightness = 0
     resolution = (0,0)
@@ -69,6 +71,10 @@ class PIMON:
         return 0
 
 
+    def get_berometer_data(self):
+
+        return 0
+
     def take_picture(self):
         print("Take Picture.")
         image_name = self.tmp_dir + "Image_" + str(self.loop_int) + ".jpg"
@@ -76,22 +82,33 @@ class PIMON:
         return image_name
 
 
-    def write_text_to_image(self):
-        image = self.take_picture()
-        img = Image.open( image)
-        filename = os.path.basename(image)
-        draw = ImageDraw.Draw(img)
-        #font = ImageFont.truetype(<font-file>, <font-size>)
-        font = ImageFont.truetype(self.text_font, 40)
+    def get_data_trigger(self):
+        if self.take_picture_flag:
+            image = self.take_picture()
+            img = Image.open( image)
+            filename = os.path.basename(image)
+            draw = ImageDraw.Draw(img)
+            #font = ImageFont.truetype(<font-file>, <font-size>)
+            font = ImageFont.truetype(self.text_font, 40)
+
         data = self.get_temp_humidity_data()
         if data:
             self.insert_data(data)
-            #draw.text((x, y),"Sample Text",(r,g,b))
-            draw.text((10, 0), "Humidity:       " + str(data[0]) + "%" ,self.text_color, font=font)
-            draw.text((10, 60), "Temperature: " + str(data[1]) + "C / " + str(data[2]) + "F" ,self.text_color, font=font)
-            img.save(self.output_path + filename, quality=self.jpeg_quality)
-            os.remove(image)
-            print("Saved Image: " + self.output_path + filename)
+            if self.verbose:
+                ts = time.time()
+                timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+                print("TimeStamp: " + timestamp)
+                print("Humidity:       " + str(data[0]) + "%")
+                print("Temperature: " + str(data[1]) + "C / " + str(data[2]) + "F")
+
+            if self.take_picture_flag:
+                #draw.text((x, y),"Sample Text",(r,g,b))
+                draw.text((10, 0), "Humidity:       " + str(data[0]) + "%" ,self.text_color, font=font)
+                draw.text((10, 60), "Temperature: " + str(data[1]) + "C / " + str(data[2]) + "F" ,self.text_color, font=font)
+                img.save(self.output_path + filename, quality=self.jpeg_quality)
+                os.remove(image)
+                print("Saved Image: " + self.output_path + filename)
             return 1
         else:
             print("Data return error.")
--- monitor.py
+++ monitor.py
@@ -1,5 +1,5 @@
 from PiWeMConfig.PiWeMConfig import PiWeMConfig
-from pimon import pimon
+from PiWeM import PiWeM
 import sys, time
 
 PWMConfig = PiWeMConfig()
@@ -11,17 +11,19 @@ resolution = (int(res_split[0]),int(res_split[1]))
 text_color_split = settings['text_color'].split(",")
 text_color = (int(text_color_split[0]), int(text_color_split[1]), int(text_color_split[2]))
 
-mon = pimon.PIMON(settings['sql_host'], settings['sql_user'], settings['sql_password'], int(settings['temp_humidity_pin']), resolution, settings['output_path'], settings['tmp_dir'], brightness=int(settings['brightness']))
+mon = PiWeM.PIWEM(settings['sql_host'], settings['sql_user'], settings['sql_password'], int(settings['temp_humidity_pin']), resolution, settings['output_path'], settings['tmp_dir'], brightness=int(settings['brightness']))
 mon.jpeg_quality = int(settings['jpeg_quality'])
 mon.text_font = settings['text_font']
 mon.text_color = text_color
+mon.verbose = 1
+mon.take_picture_flag = 0
 
 i = 0
 while 1:
     print("i = %d" % i)
     mon.loop_int = i
-    if mon.write_text_to_image():  # If Sensor Data return is a Failure, do not increment, and try again.
+    if mon.get_data_trigger():  # If Sensor Data return is a Failure, do not increment, and try again.
         time.sleep(int(settings['sleep_time']))
         i = i + 1
     else:
-        time.sleep(2)
+        time.sleep(5)
