8afcde6f9ca801912d81fbb1e4fca935c5bd219a
Phil <pferland@randomintervals.com>
2016-08-28 20:18:43 -0400

    Added class for Sensor values
    Object to write sensor values to that can be used to then insert into
    the database. Easier to manage than a list.

---
+++ daemon/PiWeM/SensorValues.py
@@ -0,0 +1,21 @@
+class SensorValues(object):
+    bmp085_temp         = []
+    bmp085_pressure     = 0
+
+    bmp180_temp         = []
+    bmp180_pressure     = 0
+
+    bmp280_temp         = []
+    bmp280_pressure     = 0
+
+    dht11_temp          = []
+    dht11_humidity      = 0
+
+    dht22_temp          = []
+    dht22_humidity      = 0
+
+    thermistor          = 0
+    analog_temp_sensor  = 0
+    photoresistor       = 0
+
+    fetch_error         = 0
